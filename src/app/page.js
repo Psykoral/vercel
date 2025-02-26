679b30fe6471b762ea209edbcb991ec240d0d14d
jimoharrasutton <jim.oharrasutton@absorblms.com>
2025-02-26 11:07:58 -0800

    Updating Logo

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <Image
-                    src="/heron.png"
-                    alt="Heron logo"
-                    width={160}
-                    height={52}
+                    src="/gravity.png"
+                    alt="Logo"
+                    width={512}
+                    height={225}
                     priority
                 />
                 <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
