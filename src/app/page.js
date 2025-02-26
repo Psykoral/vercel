ddafa57f9166b7bdaf270d0e8832ff31ed3fb3a4
jimoharrasutton <jim.oharrasutton@absorblms.com>
2025-02-26 10:53:00 -0800

    Updating Logo

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <Image
-                    src="https://images.ctfassets.net/92yop6rduul9/qReDMrGZQGrkgk6Wg9yTL/785196e4b43ecbe1ad9944d34229cdaf/darkTheme_Absorb-Logo.svg?w=640"
-                    alt="Absorb logo"
+                    src="/heron.png"
+                    alt="Heron logo"
                     width={160}
                     height={52}
                     priority
