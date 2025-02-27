094305a6647fc126013944032b4510e413236426
jimoharrasutton <jim.oharrasutton@absorblms.com>
2025-02-27 11:34:38 -0800

    Adding Speed insights

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
     return (
@@ -19,6 +20,7 @@ export default function Home() {
                     </li>
                 </ol>
             </main>
+            <SpeedInsights />
             <Analytics />
         </div>
     );
